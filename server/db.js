494be8183d1c442e20dc092c9fb83889f9481f31
Tendry Tsitohaina <tsitohaina1143@gmail.com>
2026-05-21 21:07:53 +0300

    db.js suppression de valeur par defaut

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'anonymous',
-  database: process.env.MYSQLDATABASE || 'gestion_etudiants',
-  port: process.env.MYSQLPORT || 3306,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
